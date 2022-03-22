669fc0dc99d36a7b53e67d186a9dfae2054fb32f
jeetsingh32250 <102171499+jeetsingh32250@users.noreply.github.com>
2022-03-22 23:51:33 +0530

    Extra space removed

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,11 +1,7 @@
 package hello;
 import java.util.Scanner;
 public class FirstProgram {
-
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		System.out.println("hello world");
-        
 	}
-
 }
